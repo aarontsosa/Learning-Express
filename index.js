93ecc873f3951192b29f7eba48f9400bd5a07a46
Aaron Sosa <aarontsosa@gmail.com>
2017-08-10 11:01:11 -0400

    adding customer.js and some other changes

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const express = require('express');
-const app = express();
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-res.send('Hello World')
-});
 
-app.listen(3000, () => {
-    console.log('web app is hearing you on port 3000')
-})
+
--- test/test-customers-model.js
+++ test/test-customers-model.js
@@ -1,1 +1,7 @@
-code
+const expect = require('chai').expect;
+
+describe('Customers', () =>{
+    it('should be able to save the database', () => {
+        expect(1+1).to.equal(2)
+    })
+})
