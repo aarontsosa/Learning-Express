292e8eafc0fa097433d875ddbd89da1a60d687da
Aaron Sosa <aarontsosa@gmail.com>
2017-08-10 14:12:28 -0400

    added customer id to the get

--- test/test-customers-model.js
+++ test/test-customers-model.js
@@ -12,15 +12,47 @@ describe('Customers', () =>{
                 done();
             })
     });
-        it('should be able to get a customer from the database', () =>{
-            let myCustomer = new Customer();
-            myCustomer
-                .get(1)
-                .then((result) => {
-                    console.log(myCustomer.name)
-                    console.log(myCustomer.email)
-                    console.log(myCustomer.address)
-                    done();
-                });
-        });
+    it('should be able to get a customer from the database', (done) =>{
+        Customer
+            .get(1)
+            .then((myCustomer) => {
+                done();
+            });
+    });
+    it('should save, provide an id, and then get via id', (done) => {
+        let data = ['ronald mcdonald', 'r@micky-dees.com', 'everywhere', 'yes']
+        let c1 = new Customer(...data);
+        c1.save()
+            .then((result) => {
+                let customer_id = result.customer_id
+                Customer.get(customer_id)
+                .then((c2) => {
+                    expect(c2.name).to.equal(data[0])
+                    expect(c2.email).to.equal(data[1])
+                    expect(c2.address).to.equal(data[2])
+                })
+                done()
+            })
+    })
+
+    it('should have a customer_id when we retrieve from db', (done) => {
+        let data = ['ronald mcdonald', 'r@micky-dees.com', 'everywhere', 'yes']
+        let c1 = new Customer(...data);
+        c1.save()
+            .then((result) => {
+                let customer_id = result.customer_id
+                Customer.get(customer_id)
+                .then((c2) => {
+                    expect(c2.customer_id).to.equal(customer_id)
+                })
+                done()
+            })
+            .catch(console.log)
+    })
+
+    it('should update a user and retain the new values', (done) => {
+
+
+        done()
+    })
 });
